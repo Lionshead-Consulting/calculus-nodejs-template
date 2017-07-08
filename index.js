94acd27e98fcf962914648d656df78caecae9c7e
Michael Iglesias <mike.iglesias11@gmail.com>
2017-07-08 12:04:20 -0400

    npm package remove imports

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 // @flow
 
 import AWS from 'aws-sdk';
-import _ from 'lodash';
-import R from 'ramda';
 import axios from 'axios';
-import co from 'co';
-const Maybe = require('folktale/maybe');
-const Result = require('folktale/result');
-const {task} = require('folktale/concurrency/task');
 
 exports.handler = (event, context, callback) => {
     console.log('Hello, %FunctionName%');
