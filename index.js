1edaa11575c360a0682348c95bbc23b14b905c51
Michael Iglesias <mike.iglesias11@gmail.com>
2017-08-20 14:19:44 -0400

    added NODE_ENV const

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 import AWS from 'aws-sdk';
 import axios from 'axios';
 
+const NODE_ENV = process.env.NODE_ENV || 'dev';
+
 exports.handler = (event: any, context: any, callback: Function) => {
     console.log('Hello, %FunctionName%');
 
